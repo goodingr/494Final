5f0bbf0887d014004144642062d09e794a2be275
Harrison Noh <hyuntaek@yahoo.com>
2016-03-20 19:56:34 -0400

    added falling floor mechanic

---
+++ Assets/Scripts/FallingFloor.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FallingFloor : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void onCollisionEnter(Collider col) {
+		if (col.gameObject.tag == "Player") {
+			GetComponent<Rigidbody> ().useGravity = true;
+		}
+	}
+}
