9a019f1044686cf5110525d771b5f1ea3364a707
Harrison Noh <hyuntaek@yahoo.com>
2016-03-28 21:06:16 -0400

    changed sprite

---
+++ Assets/Smell.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Smell : MonoBehaviour {
+
+	public float speed = 3f;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		float step = speed * Time.deltaTime;
+		Vector3 destination = new Vector3 (Player.S.transform.position.x, Player.S.transform.position.y, Player.S.transform.position.z + 3f);
+		transform.position = Vector3.MoveTowards(transform.position, destination, step);
+	}
+}
