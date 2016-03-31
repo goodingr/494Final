3f4766be94095eb5d5f977d54dabaa84247a5737
levinsonalex <levinsonalex@gmail.com>
2016-03-31 02:09:02 -0400

    Fluid changes.

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -15,10 +15,11 @@ public class CameraFollow : MonoBehaviour {
 
     public Vector3 regularGravityOffset;
     public Vector3 inverseGravityOffset;
+    public float verticalLensShiftOffset;
+
 
 	// Use this for initialization
 	void Start () {
-        SetObliqueness(0, .2f);
         player = Player.S.gameObject;
         offset = regularGravityOffset;
         originOffset = transform.position;
@@ -27,6 +28,7 @@ public class CameraFollow : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+        SetObliqueness(0, verticalLensShiftOffset);
         u = u + Time.deltaTime;
         Vector3 playerSizeCameraChange = new Vector3(0, 0, -Vector3.Magnitude(player.transform.localScale)) * playerSizeCameraChangeMultiplier;
         if (u < interpTime) {
