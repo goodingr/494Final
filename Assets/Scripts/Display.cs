b2c842adec62aef02604c9c3b82950ca7cfa7223
goodingr <goodingr@umich.edu>
2016-04-04 22:23:50 -0400

    Timer/Display prefab, Tuned the Bounce level

---
+++ Assets/Scripts/Display.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Display : MonoBehaviour {
+	public Text timerLabel;
+
+	private float time = 0f;
+
+	void FixedUpdate() {
+		time += Time.deltaTime;
+
+		var minutes = time / 60; 
+		var seconds = time % 60;
+
+		//update the label value
+		timerLabel.text = string.Format ("{0:0}:{1:00}", minutes, seconds);
+
+	}
+
+}
+
