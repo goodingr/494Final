f310d56bb3c9548cefe32f7fb35e04a707b33de3
Harrison Noh <hyuntaek@yahoo.com>
2016-04-10 01:12:34 -0400

    added new mechanic

---
+++ Assets/_Scripts/BrickWall.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BrickWall : MonoBehaviour {
+
+	public int bricks = 0;
+	public GameObject brick;
+	public float scale = 0;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+
+	void OnCollisionEnter (Collision coll) {
+		if (coll.gameObject.tag == "Player" && 
+			coll.gameObject.transform.localScale.x >= scale) {
+			Destroy (gameObject);
+			for (int i = 0; i < bricks; i++) {
+				Instantiate (brick, transform.position, Quaternion.identity);
+			}
+		}
+	}
+}
---
+++ Assets/_Scripts/Bricks.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bricks : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		StartCoroutine(DestroyBrick());
+	}
+
+	IEnumerator DestroyBrick() {
+		yield return new WaitForSeconds(2);
+		Destroy (gameObject);
+	}
+}
