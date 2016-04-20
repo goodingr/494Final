2f83f378b41db491369917fb4d78e34e427ea707
Harrison Noh <hyuntaek@yahoo.com>
2016-04-20 18:56:23 -0400

    added harrison level

---
+++ Assets/_Scripts/Balloon.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Balloon : MonoBehaviour {
+
+	public static Balloon B;
+	public Vector3 target;
+	public float speed;
+	public bool triggered = false;
+
+	// Use this for initialization
+	void Start () {
+		B = this;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(triggered) {
+			float step = speed * Time.deltaTime;
+			transform.position = Vector3.MoveTowards(transform.position, target, step);
+		}
+	}
+
+	public void setTrigger() {
+		triggered = true;
+	}
+}
---
+++ Assets/_Scripts/Fire.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Fire : MonoBehaviour {
+
+	public GameObject smoke;
+	public GameObject pickdown;
+	public AudioSource audio;
+	public bool triggered;
+	public GameObject triggerObject;
+
+	// Use this for initialization
+	void Start () {
+		audio = GetComponent<AudioSource> ();
+		triggered = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+
+	void OnTriggerEnter(Collider col) {
+		print ("collision");
+		if (col.gameObject.tag == "Player" && !triggered) {
+			Instantiate (smoke, transform.position, Quaternion.identity);
+			audio.Play ();
+			triggered = true;
+			triggerObject.GetComponent<Balloon> ().setTrigger ();
+			//Destroy (gameObject);
+		}
+	}
+}
