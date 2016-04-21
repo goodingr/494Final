33d96c2f3dcf4bf5a6141cae9277aea2c6c240ee
levinsonalex <levinsonalex@gmail.com>
2016-04-21 15:31:30 -0400

    Fixed bug.

--- Assets/_Scripts/Main.cs
+++ Assets/_Scripts/Main.cs
@@ -20,7 +20,7 @@ public class Main : MonoBehaviour {
 		}
 	}
 	public void PlayGame() {
-		SceneManager.LoadScene ("Scene_1 (Intro)");
+		SceneManager.LoadScene ("Scene_1");
 	}
     public void LoadNextLevel() {
 		// Reset gravity
